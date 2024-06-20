a175cae8c559bbf12618022bbfd297560ec282b2
Andre Lennard Uy <andrelennard.uy@sun-asterisk.com>
2024-06-20 14:56:53 +0800

    fetch blogs as home page

--- fe/app/page.js
+++ fe/app/page.js
@@ -1,8 +1,28 @@
+export default async function Home() {
+  const res = await fetch('http://localhost:8000/api/blog');
+  const blogs = await res.json();
 
+  const { data, pagination } = blogs;
 
-export default function Home() {
   return (
     <main>
+      <h1>Blogs</h1>
+      <ul>
+        {data.map((blog) => (
+          <li key={blog.id}>
+            <h2>{blog.title}</h2>
+            <p>{blog.blog}</p>
+            <p>Author: {blog.user.name}</p>
+            <p>Created At: {blog.createdAt}</p>
+          </li>
+        ))}
+      </ul>
+      <div>
+        <p>Total: {pagination.total}</p>
+        <p>Page: {pagination.page}</p>
+        <p>Page Size: {pagination.pageSize}</p>
+        <p>Total Pages: {pagination.totalPages}</p>
+      </div>
     </main>
   );
 }
