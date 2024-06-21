de3ed94d9d4e2dd10d2bdb2c09c77629d6ae5e19
Andre Lennard Uy <andrelennard.uy@sun-asterisk.com>
2024-06-21 14:11:16 +0800

    imporve navbar

--- fe/app/component/navbar.js
+++ fe/app/component/navbar.js
@@ -17,7 +17,6 @@ const navbar = () => {
               const user = await res.json();
               setAuthUser(user);
               setIsLogin(true);
-              console.log('User fetched successfully:', user);
             } else {
               console.log('Failed to fetch user. Status:', res.status);
               setIsLogin(false);
@@ -52,19 +51,29 @@ const navbar = () => {
     
  return (
  <>
-    <nav class="navbar fixed-top bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <Link class="navbar-brand" href="/">Blogs</Link>
-            {isLogin ? (
-                <>
-                    <button className="navbar-brand" onClick={logout}>Logout</button>
-                    <Link className="navbar-brand" href="/">Profile</Link>
-                </>
-            ) : (
-                <>
-                    <Link class="navbar-brand" href="/users">Login</Link>
-                </>
-            )}
+            <Link class="navbar-brand" href="/">Blogs App</Link>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <Link class="nav-link active" aria-current="page" href="/">Home</Link>
+                    </li>
+                </ul>
+                {isLogin ? (
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Profile
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a className="navbar-brand" onClick={logout}>Logout</a></li>
+                            <li><Link className="navbar-brand" href="/">Profile</Link></li>
+                        </ul>
+                    </li>
+                ) : (
+                    <Link class="navbar-brand" href="/users">Login</Link>            
+                )}
+            </div>
         </div>
     </nav>
  </>
